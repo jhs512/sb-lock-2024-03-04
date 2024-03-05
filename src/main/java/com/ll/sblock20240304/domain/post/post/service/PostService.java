7e193def8c08e9b365d42df397f7e5c97a25bcb6
jhs512 <jangka512@gmail.com>
2024-03-05 10:27:47 +0900

    work

--- src/main/java/com/ll/sblock20240304/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/sblock20240304/domain/post/post/service/PostService.java
@@ -41,5 +41,9 @@ public class PostService {
     public Optional<Post> findWithShareLockById(long id) {
         return postRepository.findWithShareLockById(id);
     }
+
+    public Optional<Post> findWithWriteLockById(long id) {
+        return postRepository.findWithWriteLockById(id);
+    }
 }
 
